11f2af30b76147684f1e1ae1a33ed45b8b3a8989
Maksim <maksim20002905@gmail.com>
2024-11-10 07:05:07 +0500

    adaptation

--- script.js
+++ script.js
@@ -36,6 +36,44 @@ function twoDigits(num) {
         return num
     }
 }
+function nextDay() {
+    let tomorow = new Date()
+    tomorow.setDate( tomorow.getDate() + 1)
+    tomorow.setHours( 0, 0, 0, 0)
+    return tomorow
+}
+
+function calcTimerS() {
+    let today = new Date()
+    let saleDeadline = nextDay()
+    let delta = (saleDeadline - today)/1000
+    delta = Math.trunc(delta)
+
+    console.log(delta)
+
+    let hours = Math.trunc(delta/3600)
+    let minets = Math.trunc(delta/60) - hours*60
+    let seconds = delta - hours*60*60 - minets*60
+    let hoursElem = document.getElementById("timerHoursTwo")
+    let minetsElem = document.getElementById("timerMinetsTwo")
+    let secondsElem = document.getElementById("timerSecondsTwo")
+    hoursElem.innerHTML = twoDigits(hours)
+    minetsElem.innerHTML = twoDigits(minets)
+    secondsElem.innerHTML = twoDigits(seconds)
+
+}
+
+calcTimerS()
+setInterval(calcTimerS, 1000)
+
+
+function twoDigits(num) {
+    if(num<10) {
+        return "0" + num
+    } else {
+        return num
+    }
+}
 
 
 
